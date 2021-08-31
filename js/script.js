7cf1a86c4011f3789398c8ffecce1da7b7ae7a5f
camillamanzo <camillasouthafrica@gmail.com>
2021-08-31 16:00:16 +0200

    finalized palindrome exercise

--- js/script.js
+++ js/script.js
@@ -2,7 +2,37 @@
  *      Palidroma
  * Chiedere all’utente di inserire una parola
  * Creare una funzione per capire se la parola inserita è palindroma
- * 
+*/
+
+
+
+
+let parola = prompt("Scriva una parola.")                    // Chiedo una parola all'utente
+
+function isPalindrome(parola1) {                             // Inizio la funzione
+
+    let parolaReverse = ""                                   // Inizializzo una variabile vuota da riempire con le lettere (come fosse un array)
+
+    for (i = parola1.length - 1; i >= 0; i--){               // Con il ciclo for (al contrario) trovo le lettere da mettere nella variabile
+        // console.log(parola1[i]);
+        parolaReverse += parola1[i];
+    }
+
+    if (parola1 === parolaReverse){                          // Verifico che la parola inserita sia uguale al contrario
+        return true;
+    }
+    return false;
+}
+
+if (isPalindrome (parola) ){
+    console.log("la tua parola: " + parola +  " è un palindromo.")
+    }else{
+        console.log("la tua parola non è un palindromo.");
+}
+
+
+
+/*
  *      Pari e Dispari
  * L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
  * Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
@@ -13,18 +43,5 @@
 
 
 
-//chiedo una parola all'utente
-let parola = prompt("Scriva una parola.")
 
-function isPalindrome(parola1, parola2) {
-    if (parola1.value === parola2.value.reverse){
-        return true;
-    }
-    return false;
-}
 
-if (isPalindrome (parola1) ){
-    console.log("la tua parola:" + " " + parola + " " "è un palindromo.")
-    }else{
-        console.log("la tua parola non è un palindromo.");
-}
